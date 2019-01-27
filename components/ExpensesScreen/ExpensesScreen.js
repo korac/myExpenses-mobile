0b51780f1c97fcb4a9158d5242a7522807f2f22f
Kristijan Korać <korac.kristijan@gmail.com>
2019-01-27 22:13:54 +0100

    Render expenses list

--- components/ExpensesScreen/ExpensesScreen.js
+++ components/ExpensesScreen/ExpensesScreen.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableHighlight, FlatList } from 'react-native';
+import moment from 'moment';
 
-import MeHeader from '../Shared/MeHeader';
 import styles from './ExpensesScreen.styles';
 
+import { expenses } from './Expenses.data';
+
 class ExpensesScreen extends Component {
+  renderExpenseItem(item) {
+    return (
+      <View style={styles.expenseItem}>
+        <View style={styles.expenseInfo}>
+          <Text style={styles.expenseAmount}>{item.amount}</Text>
+          <Text style={styles.expenseDate}>{moment(item.date).format('DD MMM YYYY')}</Text>
+        </View>
+        <View style={styles.expenseCategory}>
+          <Text>{item.expense_category_id}</Text>
+        </View>
+      </View>
+    );
+  }
+
   render() {
     // const { navigate } = this.props.navigation;
 
     return (
       <View style={styles.appContainer}>
-        {/*<Text>Expenses {this.props.name}</Text>*/}
-        {/*<TouchableOpacity style={styles.button} onPress={() => navigate('Home')}>*/}
-        {/*<Text style={{ color: '#fff' }}>A</Text>*/}
-        {/*</TouchableOpacity>*/}
+        <FlatList
+          data={expenses}
+          renderItem={({ item }) => this.renderExpenseItem(item)}
+          keyExtractor={item => item.id.toString()}
+        />
+        <TouchableHighlight style={styles.button} onPress={() => alert('Create expense')}>
+          <Text style={{ color: '#fff' }}>A</Text>
+        </TouchableHighlight>
       </View>
     );
   }
--- components/ExpensesScreen/ExpensesScreen.styles.js
+++ components/ExpensesScreen/ExpensesScreen.styles.js
@@ -1,13 +1,31 @@
 import { StyleSheet } from 'react-native';
-import { myExpensesPurple } from '../../assets/shared-styles/general';
+import { myExpensesPurple, circularMedium, black1, black3 } from '../../assets/shared-styles/general';
 
 const styles = StyleSheet.create({
-  headerStyle: {
-    backgroundColor: myExpensesPurple
-  },
+  // headerStyle: {
+  //   backgroundColor: myExpensesPurple
+  // },
   appContainer: {
     flex: 1,
     backgroundColor: '#f9fbfc'
+  },
+  expenseItem: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+    borderBottomWidth: 1,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 14,
+    paddingRight: 14
+  },
+  expenseAmount: {
+    fontSize: 42
+  },
+  expenseDate: {
+    color: black3,
+    fontSize: 16
   },
   button: {
     display: 'flex',
